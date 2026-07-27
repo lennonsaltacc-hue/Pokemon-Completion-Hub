db36c05c03953e6fa1658d3c22c6e9608407261e
lennonsaltacc-hue <lennonsaltacc@gmail.com>
2026-07-28 01:04:54 +1200

    Update pokemon-red.js

--- pokemon-red.js
+++ pokemon-red.js
@@ -5,7 +5,8 @@ number: 1,
 name: "Bulbasaur",
 type: "Grass / Poison",
 method: "Starter",
-evolution: "Ivysaur → Venusaur"
+location: "Professor Oak",
+evolution: "Lv16 → Ivysaur → Lv32 → Venusaur"
 },
 
 {
@@ -13,7 +14,8 @@ number: 2,
 name: "Ivysaur",
 type: "Grass / Poison",
 method: "Evolution",
-evolution: "Venusaur"
+location: "Evolve Bulbasaur",
+evolution: "Lv32 → Venusaur"
 },
 
 {
@@ -21,16 +23,17 @@ number: 3,
 name: "Venusaur",
 type: "Grass / Poison",
 method: "Evolution",
+location: "Evolve Ivysaur",
 evolution: "Final Evolution"
 },
-
 
 {
 number: 4,
 name: "Charmander",
 type: "Fire",
 method: "Starter",
-evolution: "Charmeleon → Charizard"
+location: "Professor Oak",
+evolution: "Lv16 → Charmeleon → Lv36 → Charizard"
 },
 
 {
@@ -38,7 +41,8 @@ number: 5,
 name: "Charmeleon",
 type: "Fire",
 method: "Evolution",
-evolution: "Charizard"
+location: "Evolve Charmander",
+evolution: "Lv36 → Charizard"
 },
 
 {
@@ -46,16 +50,17 @@ number: 6,
 name: "Charizard",
 type: "Fire / Flying",
 method: "Evolution",
+location: "Evolve Charmeleon",
 evolution: "Final Evolution"
 },
-
 
 {
 number: 7,
 name: "Squirtle",
 type: "Water",
 method: "Starter",
-evolution: "Wartortle → Blastoise"
+location: "Professor Oak",
+evolution: "Lv16 → Wartortle → Lv36 → Blastoise"
 },
 
 {
@@ -63,7 +68,8 @@ number: 8,
 name: "Wartortle",
 type: "Water",
 method: "Evolution",
-evolution: "Blastoise"
+location: "Evolve Squirtle",
+evolution: "Lv36 → Blastoise"
 },
 
 {
@@ -71,7 +77,153 @@ number: 9,
 name: "Blastoise",
 type: "Water",
 method: "Evolution",
+location: "Evolve Wartortle",
 evolution: "Final Evolution"
+},
+
+{
+number: 10,
+name: "Caterpie",
+type: "Bug",
+method: "Catch",
+location: "Viridian Forest",
+evolution: "Lv7 → Metapod → Lv10 → Butterfree"
+},
+
+{
+number: 11,
+name: "Metapod",
+type: "Bug",
+method: "Evolution",
+location: "Evolve Caterpie",
+evolution: "Lv10 → Butterfree"
+},
+
+{
+number: 12,
+name: "Butterfree",
+type: "Bug / Flying",
+method: "Evolution",
+location: "Evolve Metapod",
+evolution: "Final Evolution"
+},
+
+{
+number: 13,
+name: "Weedle",
+type: "Bug / Poison",
+method: "Catch",
+location: "Viridian Forest",
+evolution: "Lv7 → Kakuna → Lv10 → Beedrill"
+},
+
+{
+number: 14,
+name: "Kakuna",
+type: "Bug / Poison",
+method: "Evolution",
+location: "Evolve Weedle",
+evolution: "Lv10 → Beedrill"
+},
+
+{
+number: 15,
+name: "Beedrill",
+type: "Bug / Poison",
+method: "Evolution",
+location: "Evolve Kakuna",
+evolution: "Final Evolution"
+},
+
+{
+number: 16,
+name: "Pidgey",
+type: "Normal / Flying",
+method: "Catch",
+location: "Routes 1, 2, 3",
+evolution: "Lv18 → Pidgeotto → Lv36 → Pidgeot"
+},
+
+{
+number: 17,
+name: "Pidgeotto",
+type: "Normal / Flying",
+method: "Evolution",
+location: "Evolve Pidgey",
+evolution: "Lv36 → Pidgeot"
+},
+
+{
+number: 18,
+name: "Pidgeot",
+type: "Normal / Flying",
+method: "Evolution",
+location: "Evolve Pidgeotto",
+evolution: "Final Evolution"
+},
+
+{
+number: 19,
+name: "Rattata",
+type: "Normal",
+method: "Catch",
+location: "Many Routes",
+evolution: "Lv20 → Raticate"
+},
+
+{
+number: 20,
+name: "Raticate",
+type: "Normal",
+method: "Evolution",
+location: "Evolve Rattata",
+evolution: "Final Evolution"
+},
+
+{
+number: 21,
+name: "Spearow",
+type: "Normal / Flying",
+method: "Catch",
+location: "Routes 3, 4, 9, 16",
+evolution: "Lv20 → Fearow"
+},
+
+{
+number: 22,
+name: "Fearow",
+type: "Normal / Flying",
+method: "Evolution",
+location: "Evolve Spearow",
+evolution: "Final Evolution"
+},
+
+{
+number: 23,
+name: "Ekans",
+type: "Poison",
+method: "Catch",
+location: "Routes 4, 8, 9, 11",
+evolution: "Lv22 → Arbok"
+},
+
+{
+number: 24,
+name: "Arbok",
+type: "Poison",
+method: "Evolution",
+location: "Evolve Ekans",
+evolution: "Final Evolution"
+},
+
+{
+number: 25,
+name: "Pikachu",
+type: "Electric",
+method: "Catch",
+location: "Viridian Forest / Power Plant",
+evolution: "Thunder Stone → Raichu"
+
 }
 
 ];
