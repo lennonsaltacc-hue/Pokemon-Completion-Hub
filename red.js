eb37810de9a05d03ba79b60728b373e8fb02dfcd
lennonsaltacc-hue <lennonsaltacc@gmail.com>
2026-07-28 00:32:06 +1200

    Transform Pokémon names into detailed objects
    Replaced string array of Pokémon names with objects containing detailed information for each Pokémon.

--- red.js
+++ red.js
@@ -1,16 +1,80 @@
 const pokemon = [
 
-"Bulbasaur",
-"Ivysaur",
-"Venusaur",
+{
+number: 1,
+name: "Bulbasaur",
+type: "Grass / Poison",
+method: "Starter",
+evolution: "Ivysaur → Venusaur"
+},
 
-"Charmander",
-"Charmeleon",
-"Charizard",
+{
+number: 2,
+name: "Ivysaur",
+type: "Grass / Poison",
+method: "Evolution",
+evolution: "Venusaur"
+},
 
-"Squirtle",
-"Wartortle",
-"Blastoise",
+{
+number: 3,
+name: "Venusaur",
+type: "Grass / Poison",
+method: "Evolution",
+evolution: "Final Evolution"
+},
+
+
+{
+number: 4,
+name: "Charmander",
+type: "Fire",
+method: "Starter",
+evolution: "Charmeleon → Charizard"
+},
+
+{
+number: 5,
+name: "Charmeleon",
+type: "Fire",
+method: "Evolution",
+evolution: "Charizard"
+},
+
+{
+number: 6,
+name: "Charizard",
+type: "Fire / Flying",
+method: "Evolution",
+evolution: "Final Evolution"
+},
+
+
+{
+number: 7,
+name: "Squirtle",
+type: "Water",
+method: "Starter",
+evolution: "Wartortle → Blastoise"
+},
+
+{
+number: 8,
+name: "Wartortle",
+type: "Water",
+method: "Evolution",
+evolution: "Blastoise"
+},
+
+{
+number: 9,
+name: "Blastoise",
+type: "Water",
+method: "Evolution",
+evolution: "Final Evolution"
+}
+
+];
 
 "Caterpie",
 "Metapod",
