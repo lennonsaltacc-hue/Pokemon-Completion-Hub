5e3adaa70c252488584eb2a567da83c6e926c420
lennonsaltacc-hue <lennonsaltacc@gmail.com>
2026-07-27 22:36:46 +1200

    Add progress update functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let completion = 0;
 
+
+function updateProgress(){
+
+    document.getElementById("overallProgress").style.width =
+        completion + "%";
+
+
+    document.getElementById("percentage").innerText =
+        completion + "% Complete";
+
+}
+
+
+updateProgress();
